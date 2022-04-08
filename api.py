2d55666311dd412538d9614e9b9aa061fa5af741
Nneka Tielman <ntielman@diginova.nl>
2022-04-08 23:38:19 +0200

    added API endpoints and documentation info

---
+++ api.py
@@ -0,0 +1,228 @@
+import services
+import uvicorn
+import database
+from fastapi import FastAPI, Path, HTTPException
+
+
+# database.add_db_data()  # comment out this line after db initialised
+app = FastAPI(
+    title="Hamilton API",
+    description=database.description,
+    version="0.0.1",
+    contact={
+        "name": "Nneka Tielman",
+        "url": "https://ntielman.github.io/Portfolio/#contact",
+        "email": "khalienne@gmail.com",
+    },
+    openapi_tags=database.tags_metadata,
+)
+
+################ GENERAL INFO ################
+@app.get("/", tags=["general"])
+def general_info():
+    musical_info = services.get_musical_info()
+    if not musical_info:
+        raise HTTPException(status_code=404, detail="Info not found")
+    return {"Data": musical_info}
+
+
+################ CAST INFO ################
+@app.get("/cast", tags=["cast"])
+def get_all_cast_members():
+    all_cast = services.get_cast()
+    if not all_cast:
+        raise HTTPException(status_code=404, detail="Cast not found")
+    return {"Data": all_cast}
+
+
+@app.get("/cast/id/{cast_id}", tags=["cast"])
+def get_cast_member_by_id(
+    cast_id: int = Path(
+        ..., title="Cast ID", description="The ID of the cast member to get"
+    )
+):
+    cast_member = services.get_cast_by_id(cast_id)
+    if not cast_member:
+        raise HTTPException(
+            status_code=404, detail=f"Cast with id '{cast_id}' not found"
+        )
+    return {"Data": cast_member}
+
+
+@app.get("/cast/name/{cast_name}", tags=["cast"])
+def get_cast_members_by_name(
+    cast_name: str = Path(
+        ...,
+        title="Cast Name",
+        description="The first/full/last name of the cast member to get (case insensitive)",
+    )
+):
+    cast_members = services.get_cast_by_name(cast_name)
+    if not cast_members:
+        raise HTTPException(
+            status_code=404, detail=f"Cast with name '{cast_name}' not found"
+        )
+    return {"Data": cast_members}
+
+
+@app.get("/cast/role/{role_id}", tags=["cast"])
+def get_cast_members_by_role(
+    role_id: int = Path(
+        ..., title="Role ID", description="The ID of the role/part to get"
+    )
+):
+    cast_members = services.get_cast_by_role(role_id)
+    if not cast_members:
+        raise HTTPException(
+            status_code=404, detail=f"Cast with role Id '{role_id}' not found"
+        )
+    return {"Data": cast_members}
+
+
+@app.get("/cast/random", tags=["cast"])
+def get_random_cast_members():
+    cast_members = services.get_random_cast()
+    if not cast_members:
+        raise HTTPException(status_code=404, detail="Random cast member not found")
+    return {"Data": cast_members}
+
+
+################# ROLES INFO ################
+@app.get("/roles", tags=["roles"])
+def get_all_roles():
+    all_roles = services.get_roles()
+    if not all_roles:
+        raise HTTPException(status_code=404, detail="Roles not found")
+    return {"Data": all_roles}
+
+
+@app.get("/roles/id/{role_id}", tags=["roles"])
+def get_role_by_id(
+    role_id: int = Path(
+        ..., title="Role ID", description="The ID of the role/part to get"
+    )
+):
+    role_by_id = services.get_role_by_id(role_id)
+    if not role_by_id:
+        raise HTTPException(
+            status_code=404, detail=f"Role with ID '{role_id}' not found"
+        )
+    return {"Data": role_by_id}
+
+
+@app.get("/roles/name/{role_name}", tags=["roles"])
+def get_roles_by_name(
+    role_name: str = Path(
+        ...,
+        title="Role Name",
+        description="The first/full/last name of the role/part to get (case insensitive)",
+    )
+):
+    role = services.get_roles_by_name(role_name)
+    if not role:
+        raise HTTPException(
+            status_code=404, detail=f"Roles with name '{role_name}' not found"
+        )
+    return {"Data": role}
+
+
+@app.get("/roles/cast/{cast_id}", tags=["roles"])
+def get_roles_by_cast_member(
+    cast_id: int = Path(
+        ..., title="Cast ID", description="The ID of the cast member to get"
+    )
+):
+    role = services.get_roles_by_cast(cast_id)
+    if not role:
+        raise HTTPException(
+            status_code=404, detail=f"Roles with Cast id '{cast_id}' not found"
+        )
+    return {"Data": role}
+
+
+@app.get("/roles/song/{song_id}", tags=["roles"])
+def get_roles_by_song(
+    song_id: int = Path(
+        ..., title="Song ID", description="The ID of the song/track to get"
+    )
+):
+    role = services.get_roles_by_song(song_id)
+    if not role:
+        raise HTTPException(
+            status_code=404, detail=f"Roles with Song id '{song_id}' not found"
+        )
+    return {"Data": role}
+
+
+@app.get("/roles/random", tags=["roles"])
+def get_random_roles():
+    role = services.get_random_role()
+    if not role:
+        raise HTTPException(status_code=404, detail="Random roles not found")
+    return {"Data": role}
+
+
+################ SONGS INFO ################
+@app.get("/songs", tags=["songs"])
+def get_all_songs():
+    songs = services.get_songs()
+    if not songs:
+        raise HTTPException(status_code=404, detail="Songs not found")
+    return {"Data": songs}
+
+
+@app.get("/songs/id/{song_id}", tags=["songs"])
+def get_song_by_id(
+    song_id: int = Path(
+        ..., title="Song ID", description="The ID of the song/track to get"
+    )
+):
+    song_by_id = services.get_song_by_id(song_id)
+    if not song_by_id:
+        raise HTTPException(
+            status_code=404, detail=f"Song with ID '{song_id}' not found"
+        )
+    return {"Data": song_by_id}
+
+
+@app.get("/songs/title/{song_title}", tags=["songs"])
+def get_songs_by_title(
+    song_title: str = Path(
+        ...,
+        title="Song Title",
+        description="The title of the song/track to get (case insensitive)",
+    )
+):
+    songs = services.get_songs_by_title(song_title)
+    if not songs:
+        raise HTTPException(
+            status_code=404, detail=f"Songs with title '{song_title}' not found"
+        )
+    return {"Data": songs}
+
+
+@app.get("/songs/role/{role_id}", tags=["songs"])
+def get_songs_by_role(
+    role_id: int = Path(
+        ..., title="Role ID", description="The ID of the role/part to get"
+    )
+):
+    songs = services.get_songs_by_role(role_id)
+    if not songs:
+        raise HTTPException(
+            status_code=404, detail=f"Songs with Role id '{role_id}' not found"
+        )
+    return {"Data": songs}
+
+
+@app.get("/songs/random", tags=["songs"])
+def get_random_songs():
+    # Returns one random card (or n random card if specified in parameters)
+    songs = services.get_random_song()
+    if not songs:
+        raise HTTPException(status_code=404, detail="Random songs not found")
+    return {"Data": songs}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
